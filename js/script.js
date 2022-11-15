5aea7bf36692eb55fd2c2b622c9e6aa2f5eb4857
Ilyas <il_adaev@mail.ru>
2022-11-15 15:56:02 +0600

    add js and callback

---
+++ js/script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const callFormBtn = document.querySelector('#bellBtn');
+const callForm = document.querySelector('.shape');
+const callFormCloseBtn = document.querySelector('#bellBtnClose');
+const callFormClosePlace = document.querySelector('.wrapper');
+const callFormSend = document.querySelector('.shape-form__btn');
+const callFormSendBox = document.querySelector('.send');
+const entrance = document.querySelector('#entranceOpen');
+const entranceClose = document.querySelector('#entranceClose');
+const entranceBox = document.querySelector('.entrance');
+
+callFormBtn.addEventListener('click', () => {
+  callForm.classList.add('shape-active');
+  callFormBtn.style.display = "none";
+});
+
+callFormCloseBtn.addEventListener('click', () => {
+  callForm.classList.remove('shape-active');
+  callFormBtn.style.display = "block";
+});
+
+callFormClosePlace.addEventListener('click', () => {
+  callForm.classList.remove('shape-active');
+  callFormBtn.style.display = "block";
+});
+
+callFormSend.addEventListener('click', () => {
+  callForm.classList.remove('shape-active');
+  callFormBtn.style.display = "block";
+  callFormSendBox.classList.add('send-active');
+  if (callFormSendBox.classList.contains('send-active')) {
+    setTimeout(() => {
+      callFormSendBox.classList.remove('send-active');
+    }, 1500);
+  }
+});
+
+entrance.addEventListener('click', (e) => {
+  e.preventDefault();
+  entranceBox.classList.add('entrance-active');
+});
+
+entranceClose.addEventListener('click', () => {
+  entranceBox.classList.remove('entrance-active');
+});
